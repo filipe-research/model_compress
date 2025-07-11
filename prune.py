9c82e1e0dae9a102057774c5fbf5b9f4b8010cb5
filiperobotic <filipe.rolim@gmail.com>
2025-07-11 19:23:19 -0300

    ok

--- prune.py
+++ prune.py
@@ -27,7 +27,8 @@ def prune_yolo_model(model, example_inputs, amount=0.5):
             # pruning_idxs = strategy(m.weight, amount=amount)
             # pruning_idxs = tp.strategy.ln_structured(m.weight, amount=amount, n=2, dim=0)
                         # Use the new API directly without strategy attribute
-            pruning_idxs = tp.ln_structured(m.weight, amount=amount, n=2, dim=0)
+            # pruning_idxs = tp.ln_structured(m.weight, amount=amount, n=2, dim=0)
+            pruning_idxs = tp.utils.ln_norm_pruning(m.weight, amount=amount, n=2, dim=0)
             if pruning_idxs.numel() == 0:
                 continue
             plan = DG.get_pruning_plan(m, tp.prune_conv_out_channel, pruning_idxs)
