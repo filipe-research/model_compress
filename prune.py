254aa030b12ac4630704c771e8391406d0779881
filiperobotic <filipe.rolim@gmail.com>
2025-07-11 19:19:49 -0300

    prune3

--- prune.py
+++ prune.py
@@ -1,19 +1,34 @@
 import torch
 import torch.nn.utils.prune as prune
 from ultralytics import YOLO
+import torch_pruning as tp
 
-def prune_model(model, amount=0.3):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    detection_model = model.model.to(device)
+# def prune_model(model, amount=0.3):
+#     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#     detection_model = model.model.to(device)
     
-    for name, module in detection_model.named_modules():
-        if isinstance(module, torch.nn.Conv2d):
-            # prune.l1_unstructured(module, name='weight', amount=amount)
-            prune.ln_structured(module, name='weight', amount=0.5, n=2, dim=0)  # remove 50% dos filtros
-            prune.remove(module, 'weight')
-            # print(f"Pruned layer: {name}")
-            print(f"Structured-pruned layer: {name}")
+#     for name, module in detection_model.named_modules():
+#         if isinstance(module, torch.nn.Conv2d):
+#             # prune.l1_unstructured(module, name='weight', amount=amount)
+#             prune.ln_structured(module, name='weight', amount=0.5, n=2, dim=0)  # remove 50% dos filtros
+#             prune.remove(module, 'weight')
+#             # print(f"Pruned layer: {name}")
+#             print(f"Structured-pruned layer: {name}")
     
+#     return model
+
+def prune_yolo_model(model, example_inputs, amount=0.5):
+    model.eval()
+    DG = tp.DependencyGraph().build_dependency(model, example_inputs=example_inputs)
+
+    for m in model.modules():
+        if isinstance(m, torch.nn.Conv2d):
+            strategy = tp.strategy.LNStrategy(2)  # L2 norm pruning
+            pruning_idxs = strategy(m.weight, amount=amount)
+            if pruning_idxs.numel() == 0:
+                continue
+            plan = DG.get_pruning_plan(m, tp.prune_conv_out_channel, pruning_idxs)
+            plan.exec()
     return model
 
 def main():
@@ -31,8 +46,12 @@ def main():
 
     torch_model = model.model
 
+    # Define dummy input for tracing
+    example_input = torch.randn(1, 3, 640, 640).to(device)
+
     print('Pruing model...')
-    pruned_torch_model = prune_model(torch_model, amount=0.5)
+    # pruned_torch_model = prune_yolo_model(torch_model, amount=0.5)
+    pruned_torch_model = prune_yolo_model(torch_model, example_inputs=example_input, amount=0.5)
     # prune.ln_structured(module, name='weight', amount=0.5, n=2, dim=0)  # remove 50% dos filtros
     print('Model pruned.')
 
