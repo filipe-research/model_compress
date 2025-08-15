0345768f899859d36732cdb9ced30c059aa37ce0
filiperobotic <filipe.rolim@gmail.com>
2025-08-15 08:51:50 -0300

    asdfd

--- prune_tp.py
+++ prune_tp.py
@@ -14,11 +14,25 @@ from matplotlib import pyplot as plt
 from ultralytics import YOLO, __version__
 from ultralytics.nn.modules import Detect, C2f, Conv, Bottleneck
 from ultralytics.nn.tasks import attempt_load_one_weight
-from ultralytics.yolo.engine.model import TASK_MAP
-from ultralytics.yolo.engine.trainer import BaseTrainer
-from ultralytics.yolo.utils import yaml_load, LOGGER, RANK, DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS
-from ultralytics.yolo.utils.checks import check_yaml
-from ultralytics.yolo.utils.torch_utils import initialize_weights, de_parallel
+# from ultralytics.yolo.engine.model import TASK_MAP
+# from ultralytics.yolo.engine.trainer import BaseTrainer
+# from ultralytics.yolo.utils import yaml_load, LOGGER, RANK, DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS
+# from ultralytics.yolo.utils.checks import check_yaml
+# from ultralytics.yolo.utils.torch_utils import initialize_weights, de_parallel
+
+# Ultralytics import compatibility shim (v8/v11 vs newer layouts)
+try:
+    from ultralytics.yolo.engine.model import TASK_MAP
+    from ultralytics.yolo.engine.trainer import BaseTrainer
+    from ultralytics.yolo.utils import yaml_load, LOGGER, RANK, DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS
+    from ultralytics.yolo.utils.checks import check_yaml
+    from ultralytics.yolo.utils.torch_utils import initialize_weights, de_parallel
+except Exception:
+    from ultralytics.engine.model import TASK_MAP
+    from ultralytics.engine.trainer import BaseTrainer
+    from ultralytics.utils import yaml_load, LOGGER, RANK, DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS
+    from ultralytics.utils.checks import check_yaml
+    from ultralytics.utils.torch_utils import initialize_weights, de_parallel
 
 import torch_pruning as tp
 
